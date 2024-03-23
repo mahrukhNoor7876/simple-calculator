7613d4e0c037035bccc2f5bcd67a269d8aa1e3ca
unknown <noor@example.com>
2024-03-23 21:26:04 +0500

    add the text

--- index.ts
+++ index.ts
@@ -2,8 +2,8 @@
 
 import inquirer from "inquirer";
 const answer=await inquirer.prompt([
-    { message:"Enter first number: ", type:"number", name:"firstNumber" },
-    { message:"Enter second number: ", type:"number", name:"secondNumber" },
+    { message:"Enter your first number: ", type:"number", name:"firstNumber" },
+    { message:"Enter your second number: ", type:"number", name:"secondNumber" },
     {
         message:"Select one of the operator to perform operation: ", 
         type:"list",
@@ -25,5 +25,5 @@ else if(answer.operator === "Division"){
     console.log(`${answer.firstNumber} / ${answer.secondNumber} = ${answer.firstNumber / answer.secondNumber}`);
 }
 else{
-    console.log("INVALID OPERATOR");
+    console.log("INVALID OPERATOR!");
 }
